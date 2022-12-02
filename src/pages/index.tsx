ce5206500ae546bf4fe1a969c99b68f3cc9eaa4b
Anderson <andersonfrfilho@gmail.com>
2022-12-01 23:15:07 -0300

    feat: add gitkeep

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import React, { FC } from 'react'
 import RabbitLogoSvg from "@assets/svg/RabbitMQ_logo.svg";
 import { Canvas } from "@react-three/fiber";
 import { Exchange } from "@services/rabbitmq/interfaces/exchange.interface";
 import { Queue } from "@services/rabbitmq/interfaces/queue.interface";
-import { getExchanges, getQueues } from "@services/rabbitmq/rabbitmq.api";
+import { getExchanges, getQueues } from '@services/rabbitmq/rabbitmq.api'
 import { GetStaticPropsResult, InferGetStaticPropsType } from "next";
+import { JSXElementConstructor } from "react";
 import {
   Body,
   Config,
@@ -16,75 +18,31 @@ import {
   Title
 } from "./styles";
 
-function MyBox({ position, color }: any) {
-  return (
-    <mesh position={position}>
-      <boxBufferGeometry />
-      <meshBasicMaterial color={color} />
-    </mesh>
-  );
-}
+// function MyBox ({ position, color }: ):FC<{position:string,color:string}> {
+//   return (
+//     <mesh position={position}>
+//       <boxBufferGeometry />
+//       <meshBasicMaterial color={color} />
+//     </mesh>
+//   );
+// }
 
 interface AppGetStaticInterface {
-  queues: Queue[];
-                                                exchanges: Exchange[];
+  queues: Queue[]
+  exchanges: Exchange[]
 }
 
-export default function App(
+export default function App (
   data: InferGetStaticPropsType<typeof getStaticProps>
 ) {
   return (
     <Container>
-      <Header>
-        <RabbitLogoSvg />
-        {/* <AreaLogo>
-          <LogoSvg></LogoSvg>
-        </AreaLogo>
-        <AreaTitle>
-        </AreaTitle> */}
-      </Header>
-      <Body>
-        <Config>
-          <Title>Config</Title>
-        </Config>
-        <Content>
-          <Menu>
-            <Title>Menu</Title>
-          </Menu>
-          <Painel>
-            <Canvas camera={{ position: [4, 4, 4] }}>
-              <color attach="background" args={["black"]} />
-              <MyBox position={[-1, 0, 0]} color="magenta" />
-              <MyBox position={[1, 0, 0]} color="royalblue" />
-            </Canvas>
-          </Painel>
-        </Content>
-      </Body>
-      <Body>
-        <Config>
-          <Title>Config</Title>
-        </Config>
-        <Content>
-          <Menu>
-            <Title>Menu</Title>
-          </Menu>
-          <Painel>
-            <Canvas camera={{ position: [4, 4, 4] }}>
-              <color attach="background" args={["black"]} />
-              <MyBox position={[-1, 0, 0]} color="magenta" />
-              <MyBox position={[1, 0, 0]} color="royalblue" />
-            </Canvas>
-          </Painel>
-        </Content>
-      </Body>
-      <Footer>
-        <h1>Footer</h1>
-      </Footer>
+    
     </Container>
   );
 }
 
-export async function getStaticProps(
+export async function getStaticProps (
   context: any
 ): Promise<GetStaticPropsResult<AppGetStaticInterface>> {
   const queues = await getQueues();
@@ -94,7 +52,7 @@ export async function getStaticProps(
   return {
     props: {
       queues,
-      exchanges,
-    },
-  };
+      exchanges
+    }
+  }
 }
