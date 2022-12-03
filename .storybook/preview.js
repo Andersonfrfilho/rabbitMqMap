36340ef6968ace7c437dcbb1d2b4bea78f9a1a51
Anderson Fernandes <andersonfrfilho@gmail.com>
2022-12-03 11:01:54 -0300

    feat: storybook chakra ui

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,24 +1,7 @@
-import React from 'react';
-import { addDecorator } from "@storybook/react";
-import GlobalStyle from "../src/styles/global.style";
-import { ThemeProvider } from 'styled-components';
-import theme from "../src/styles/theme";
+const theme = require('../path/to/your/theme')
 
-addDecorator((story) => (
-  <>
-    <GlobalStyle />
-    {story()}
-  </>
-));
-addDecorator((story)=>(
-  <ThemeProvider theme={theme}>{story()}</ThemeProvider>
-))
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
+  chakra: {
+    theme,
   },
 }
